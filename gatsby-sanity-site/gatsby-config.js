5927a3394d1f6587e630092c2be0ed2933c04a4b
Sea1375 <sea.dream0000@gmail.com>
2021-05-07 13:07:01 -0700

    structure

--- gatsby-sanity-site/gatsby-config.js
+++ gatsby-sanity-site/gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'dsaju8sa',
+        dataset: 'production',
+        watchMode: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
